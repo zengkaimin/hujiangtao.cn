928e15a9f1239e4d9b44e04d1b0981768291be6d
jiangtao <jiangtao.work@jiangtaoworkdeMacBook-Pro.local>
2018-05-04 14:52:35 +0800

    替换生成sidebar的方法
    添加按文件夹的目录和文件自动获取sidebar的方法

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,8 +2,38 @@
 * @Author: jiangtao
 * @Date:   2018-04-24 22:46:26
 * @Last Modified by:   jiangtao
-* @Last Modified time: 2018-04-26 00:41:15
+* @Last Modified time: 2018-05-04 14:51:15
 */
+
+const { join, basename, relative } = require('path');
+const glob = require('glob');
+
+const generateBlogSideBar = dir => {
+  const structure = {};
+  const files = glob.sync('**/*.md', { cwd: join(__dirname, '..', dir) })
+    .filter(p => basename(p) !== 'README.md')
+    .map(p => {
+      const [folder, filename] = p.split('/');
+      const file = basename(filename, '.md');
+      return [folder, file];
+    }).forEach(([folder, file]) => {
+      structure[folder] = { ...structure[folder] };
+      structure[folder][file] = join(dir, folder, file);
+    });
+
+  const folders = Object.keys(structure).sort().reverse();
+
+  return [...folders.map(folder => {
+    const filenames = Object.keys(structure[folder]).sort().reverse();
+    return {
+      title: folder,
+      collapsable: true,
+      children: [].concat(...filenames.map(filename => {
+        return [join(folder, filename)]
+      }))
+    };
+  })];
+};
 
 module.exports = {
     // base: '/vuepress/',
@@ -63,88 +93,9 @@ module.exports = {
                 ],
                 sidebar: {
                     '/post/': [
-                        {
-                            title: 'API',
-                            collapsable: true,
-                            children: [
-                                'API/2018-03-19-接口地址整理',
-                            ]
-                        },
-                        {
-                            title: 'Frontend',
-                            collapsable: true,
-                            children: [
-                                'Frontend/2017-03-28-jQuery实现任意列表或表格的排序',
-                                'Frontend/2017-12-05-Table表格中文字超出显示省略号',
-                                'Frontend/2018-01-09-JS判别设备类型',
-                                'Frontend/2018-01-21-CSS页面Flex三段式布局',
-                                'Frontend/2018-04-18-从0开始React',
-                            ]
-                        },
-                        {
-                            title: 'Git',
-                            collapsable: true,
-                            children: [
-                                'Git/2018-02-11-Git使用',
-                                'Git/2018-03-24-Git初始化项目流程',
-                            ]
-                        },
-                        {
-                            title: 'Linux',
-                            collapsable: true,
-                            children: [
-                                'Linux/2017-11-27-网站部署至服务器流程',
-                                'Linux/2017-12-05-安装supervisor',
-                                'Linux/2017-12-05-ssh配置',
-                                'Linux/2017-12-07-iptables',
-                                'Linux/2018-03-29-服务器折腾手札',
-                            ]
-                        },
-                        {
-                            title: 'Mac',
-                            collapsable: true,
-                            children: [
-                                'Mac/2018-02-05-Mac安装Python开发环境依赖',
-                                'Mac/2018-02-17-Mac下homebrew安装mysql',
-                                'Mac/2018-03-19-Mac软件清单',
-                                'Mac/2018-04-23-Mac下zsh终端使用sublime打开文件及文件夹的设置',
-                                'Mac/2018-05-02-Vim配置',
-                            ]
-                        },
-                        {
-                            title: 'MySQL',
-                            collapsable: true,
-                            children: [
-                                'MySQL/2018-04-19-MySQL优化',
-                            ]
-                        },
-                        {
-                            title: 'Nginx',
-                            collapsable: true,
-                            children: [
-                                'Nginx/2018-03-24-Nginx之location配置',
-                            ]
-                        },
-                        {
-                            title: 'Python',
-                            collapsable: true,
-                            children: [
-                                'Python/2017-01-19-一个Python简单项目框架的建立流程',
-                                'Python/2017-11-24-Python项目生成requirements_txt',
-                                'Python/2017-12-14-Python实现定时任务初步探索',
-                                'Python/2018-01-23-Python项目架构的思考',
-                                'Python/2018-04-09-Python-Tornado后台服务器跨域设置示例',
-                            ]
-                        },
-                        {
-                            title: 'TensorFlow',
-                            collapsable: true,
-                            children: [
-                                'TensorFlow/2018-05-02-TensorFlow',
-                            ]
-                        },
-                    ],
-                }
+                        ...generateBlogSideBar('/post'),
+                        ],
+                },
                 // search: false,
                 // searchMaxSuggestions: 10,
             },
@@ -171,111 +122,11 @@ module.exports = {
                     },
                 ],
                 sidebar: {
-                    '/zh/post/': genPostSidebarConfig()
+                    '/zh/post/': [
+                        ...generateBlogSideBar('/post'),
+                        ],
                 }
             }
         }
     }
-}
-
-function genSidebarConfig (tTitle) {
-  return [
-    {
-      title: tTitle,
-      collapsable: false,
-      children: [
-        '',
-        '2017-01-19-一个Python简单项目框架的建立流程.md',
-        '2017-03-28-jQuery实现任意列表或表格的排序.md',
-        '2017-12-05-ssh配置.md',
-        '2018-01-09-JS判别设备类型.md',
-        '2018-03-24-Git初始化项目流程.md',
-      ]
-    }
-  ]
-}
-
-function genPostSidebarConfig () {
-    return [
-        {
-            title: 'API',
-            collapsable: true,
-            children: [
-                'API/2018-03-19-接口地址整理',
-            ]
-        },
-        {
-            title: 'Frontend',
-            collapsable: true,
-            children: [
-                'Frontend/2017-03-28-jQuery实现任意列表或表格的排序',
-                'Frontend/2017-12-05-Table表格中文字超出显示省略号',
-                'Frontend/2018-01-09-JS判别设备类型',
-                'Frontend/2018-01-21-CSS页面Flex三段式布局',
-                'Frontend/2018-04-18-从0开始React',
-            ]
-        },
-        {
-            title: 'Git',
-            collapsable: true,
-            children: [
-                'Git/2018-02-11-Git使用',
-                'Git/2018-03-24-Git初始化项目流程',
-            ]
-        },
-        {
-            title: 'Linux',
-            collapsable: true,
-            children: [
-                'Linux/2017-11-27-网站部署至服务器流程',
-                'Linux/2017-12-05-安装supervisor',
-                'Linux/2017-12-05-ssh配置',
-                'Linux/2017-12-07-iptables',
-                'Linux/2018-03-29-服务器折腾手札',
-            ]
-        },
-        {
-            title: 'Mac',
-            collapsable: true,
-            children: [
-                'Mac/2018-02-05-Mac安装Python开发环境依赖',
-                'Mac/2018-02-17-Mac下homebrew安装mysql',
-                'Mac/2018-03-19-Mac软件清单',
-                'Mac/2018-04-23-Mac下zsh终端使用sublime打开文件及文件夹的设置',
-                'Mac/2018-05-02-Vim配置',
-            ]
-        },
-        {
-            title: 'MySQL',
-            collapsable: true,
-            children: [
-                'MySQL/2018-04-19-MySQL优化',
-            ]
-        },
-        {
-            title: 'Nginx',
-            collapsable: true,
-            children: [
-                'Nginx/2018-03-24-Nginx之location配置',
-            ]
-        },
-        {
-            title: 'Python',
-            collapsable: true,
-            children: [
-                'Python/2017-01-19-一个Python简单项目框架的建立流程',
-                'Python/2017-11-24-Python项目生成requirements_txt',
-                'Python/2017-12-14-Python实现定时任务初步探索',
-                'Python/2018-01-23-Python项目架构的思考',
-                'Python/2018-04-09-Python-Tornado后台服务器跨域设置示例',
-            ]
-        },
-        {
-            title: 'TensorFlow',
-            collapsable: true,
-            children: [
-                'TensorFlow/2018-05-02-TensorFlow',
-            ]
-        },
-    ]
 }
