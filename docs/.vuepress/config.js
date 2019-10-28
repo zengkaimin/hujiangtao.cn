b2121f733f8507d57e5c9db8d2950f32349ab81d
jiangtao <jiangtaohu@alumni.hust.edu.cn>
2019-10-28 17:06:44 +0800

    Update: favicon icon change

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -58,14 +58,14 @@ module.exports = {
         // }
     },
     head: [
-        ['link', { rel: 'icon', href: `/favicon.png` }],
+        ['link', { rel: 'icon', href: `/icons/v1/icon-48x48.png` }],
         ['link', { rel: 'manifest', href: '/manifest.json' }],
         ['meta', { name: 'theme-color', content: '#3eaf7c' }],
         ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
         ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-        ['link', { rel: 'apple-touch-icon', href: `/favicon.png` }],
+        ['link', { rel: 'apple-touch-icon', href: `icons/v1/icon-512x512.png` }],
         // ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-        ['meta', { name: 'msapplication-TileImage', content: '/favicon.png' }],
+        ['meta', { name: 'msapplication-TileImage', content: '/icons/v1/icon-512x512.png' }],
         ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
     ],
     themeConfig: {
