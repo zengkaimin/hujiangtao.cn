f28175071b5d378319d652eda2418a696513c501
jiangtao <jiangtaohu@alumni.hust.edu.cn>
2019-10-28 14:01:27 +0800

    Add: backup.sh & backup files

---
+++ backup.sh
@@ -0,0 +1,7 @@
+#back up dist files to backup with name of $(sitename + date + version)
+#tar -zcvf www.hujiangtao.cn_$(date +"%Y-%m-%d")_v2.tar ../dist
+backup_from=docs/.vuepress/dist
+backup_to=docs/.vuepress/backup/www.hujiangtao.cn_$(date +"%Y-%m-%d")_v2.tar
+tar -zcvf $backup_to $backup_from
+echo "=========="
+echo "back up to $backup_to complete!"
